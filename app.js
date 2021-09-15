c73b7047e752a372a617fc420865f90594068019
nelidita <nelidavalero06@gmail.com>
2021-09-15 16:46:52 -0500

    reconstruyendo la imagen en un div por cada pixel

--- app.js
+++ app.js
@@ -7,83 +7,46 @@ const app = (function () {
         context = canvas.getContext('2d'),
         img = document.getElementById('img');
 
-    // const canvasReconstructionImage = document.createElement('canvas'),
-    //     contextReconstruction = canvasReconstructionImage.getContext('2d');
-
-
     canvas.width = img.width;
     canvas.height = img.height;
     context.drawImage(img, 0, 0);
 
     const imageData = context.getImageData( 0, 0, img.width, img.height );
 
-    // public.getImgData = () => {
-    //     return context.getImageData(0, 0, canvas.width, canvas.height);
-    // };
-
-    // //Reconstrucción de la imagen
-
+    //Reconstrucción de la imagen
     public.reconstruction = () => {
-        // const imageData = app.getImgData(),
            pixels = imageData.data,
             numPixels = imageData.width * imageData.height;
 
             console.log(pixels)
             console.log(numPixels)
+            console.log(imageData.width)
+            console.log(imageData.height)
 
         for (let i = 0; i < numPixels; i += 4) {
             
             const r = pixels[i];
             const g = pixels[i + 1];
             const b = pixels[i + 2];
-            // const a = pixels[i + 3];
-            // console.log(r,g,b)
             
-            // pixels[i] = r;
-            // pixels[i + 1] = g;
-            // pixels[i + 2] = b;
-            // pixels[i + 3] = b;
-
-            const x = canvas.width;
-            const y = canvas.height;
-            const off = (y * imageData.width + x) * 4;
-
-            pixels[off] = r;
-            pixels[off + 1] = g;
-            pixels[off + 2] = b;
-            pixels[off + 3] = 255;
+            pixels[i] = r;
+            pixels[i + 1] = g;
+            pixels[i + 2] = b;
             
             const container = document.getElementById("container");
             const div = document.createElement("div")
+            div.className = "divPixel"
             container.appendChild(div);
-            div.style.backgroundColor = (r, g, b); 
-            // div.style.backgroundColor = "red"
+            container.style.width = imageData.width + "px";
+            container.style.height = imageData.height + "px";
+
+            div.style.backgroundColor = "rgb("+r+","+g+","+b+")"
+            
+            console.log(div.length)
         }
 
-        // context.putImageData(imageData, 300, 300);
+        context.putImageData(imageData, 0, 0);
     };
-
-    // public.sepia = function () {
-    //     // var imageData = app.getImgData(),
-    //         pixels = imageData.data,
-    //         numPixels = imageData.width * imageData.height;
-
-    //     for ( var i = 0; i < numPixels; i++ ) {
-    //         var r = pixels[ i * 4 ];
-    //         var g = pixels[ i * 4 + 1 ];
-    //         var b = pixels[ i * 4 + 2 ];
-
-    //         pixels[ i * 4 ] = 255 - r;
-    //         pixels[ i * 4 + 1 ] = 255 - g;
-    //         pixels[ i * 4 + 2 ] = 255 - b;
-
-    //         pixels[ i * 4 ] = ( r * .393 ) + ( g *.769 ) + ( b * .189 );
-    //         pixels[ i * 4 + 1 ] = ( r * .349 ) + ( g *.686 ) + ( b * .168 );
-    //         pixels[ i * 4 + 2 ] = ( r * .272 ) + ( g *.534 ) + ( b * .131 );
-    //     }
-
-    //     context.putImageData( imageData, 0, 0 );
-    // };
 
     // public.save = function () {
     //     var link = window.document.createElement( 'a' ),
@@ -102,9 +65,4 @@ const app = (function () {
 }());
 
 app.reconstruction();
-// app.sepia();
 // app.save();
-// app.contrast()
-// console.log(app.reconstruction());
-// console.log(app.getImgData().data.length);
-// console.log(app.getImgData().data);
